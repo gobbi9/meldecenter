545d5cb0f519ddf7e4aec333bc0ad6b97a6677a1
Guilherme Gobbi <g@gobbi.info>
2025-12-30 06:24:56 +0100

    Improve ftp testcontainers config

--- src/test/kotlin/coding/challenge/meldecenter/testconfig/MeldecenterSpringBootTest.kt
+++ src/test/kotlin/coding/challenge/meldecenter/testconfig/MeldecenterSpringBootTest.kt
@@ -9,16 +9,4 @@ import java.lang.annotation.Inherited
 @Inherited
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration::class)
-@org.springframework.test.context.ContextConfiguration(initializers = [FtpInitializer::class])
 annotation class MeldecenterSpringBootTest
-
-class FtpInitializer : org.springframework.context.ApplicationContextInitializer<org.springframework.context.ConfigurableApplicationContext> {
-    override fun initialize(applicationContext: org.springframework.context.ConfigurableApplicationContext) {
-        TestcontainersConfiguration.ftpContainerInstance.start()
-        org.springframework.test.context.support.TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
-            applicationContext,
-            "ftp.host=" + TestcontainersConfiguration.ftpContainerInstance.host,
-            "ftp.port=" + TestcontainersConfiguration.ftpContainerInstance.getMappedPort(21)
-        )
-    }
-}
--- src/test/kotlin/coding/challenge/meldecenter/testconfig/TestcontainersConfiguration.kt
+++ src/test/kotlin/coding/challenge/meldecenter/testconfig/TestcontainersConfiguration.kt
@@ -4,34 +4,30 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.FixedHostPortGenericContainer
-import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
+import org.testcontainers.utility.MountableFile
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    fun postgresContainer(): PostgreSQLContainer<*> {
-        return PostgreSQLContainer(DockerImageName.parse("postgres:17.7-alpine"))
-    }
+    fun postgresContainer(): PostgreSQLContainer<*> =
+        PostgreSQLContainer(DockerImageName.parse("postgres:17.7-alpine"))
 
     @Bean
-    fun ftpContainer(): GenericContainer<*> {
-        return ftpContainerInstance
-    }
-
-    companion object {
-        val ftpContainerInstance: FixedHostPortGenericContainer<*> =
+    fun ftpContainer(): FixedHostPortGenericContainer<*> =
         FixedHostPortGenericContainer("delfer/alpine-ftp-server:latest")
             .withEnv("USERS", "admin|admin")
-            .withFixedExposedPort(21, 21)
-            .withFixedExposedPort(21000, 21000)
             .withEnv("MIN_PORT", "21000")
             .withEnv("MAX_PORT", "21000")
-            .withFileSystemBind("src/test/resources/ausgehend/ftp", "/ftp/admin")
+            .withFixedExposedPort(21, 21)
+            .withFixedExposedPort(21000, 21000)
+            .withCopyToContainer(
+                MountableFile.forHostPath("src/test/resources/ausgehend/ftp"),
+                "/ftp/admin"
+            )
             .waitingFor(Wait.forListeningPort())
-    }
 }
